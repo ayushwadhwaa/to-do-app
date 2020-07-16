a885ef6b8ac7f90a5a68958f857b06a301d1aa6e
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-16 22:34:14 +0530

    Updated GoalItem.js

---
+++ Components/GoalItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
+const GoalItem = props =>{
+    return(
+        <View style={styles.listItem}>
+            <Text></Text>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    listItem: {
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ccc',
+        borderColor: 'black',
+        borderWidth: 1
+    }
+});
+export default GoalItem;
