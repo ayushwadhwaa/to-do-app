0963e8a715f55b6be9a67ea9dece68f78d589960
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-17 01:37:41 +0530

    Added Alert for deleteGoal

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
+import {View, Text, StyleSheet, Button, Alert} from 'react-native';
 const GoalItem = props =>{
+    const deleteGoal = () => {
+        Alert.alert("Alert!","Are you sure you want to delete this goal!",[{
+                text:"Cancel",
+                style:"cancel"
+            },{
+                text:"Yes",
+                style:'destructive',
+                onPress:props.onDelete.bind(this, props.title)
+            }   
+    ]);
+    } 
     return(
         <View style={styles.listItems}>
             <View style={styles.listTitle}>
                 <Text>{props.title}</Text>
             </View>
             <View style={styles.delButton}>
-                <Button title="X" color="red" onPress={props.onDelete.bind(this, props.title)}/>
+                <Button title="X" color="red" onPress={deleteGoal}/>
             </View>   
         </View>
     );
