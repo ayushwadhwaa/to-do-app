2a5b758e6d3c8b0167e42db6e7327ca9f9d54c4c
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-17 01:23:11 +0530

    Added delete button in GoalItem.js

--- Components/GoalItem.js
+++ Components/GoalItem.js
@@ -2,18 +2,36 @@ import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 const GoalItem = props =>{
     return(
-        <View style={styles.listItem}>
-            <Text>{props.title}</Text>
+        <View style={styles.listItems}>
+            <View style={styles.listTitle}>
+                <Text>{props.title}</Text>
+            </View>
+            <View style={styles.delButton}>
+                <Button title="X" color="red"/>
+            </View>   
         </View>
     );
 };
 const styles = StyleSheet.create({
-    listItem: {
+    listItems: {
+        flexDirection: 'row',
+        justifyContent:"space-between",
+        alignItems: 'center',
         padding: 10,
-        marginVertical: 10,
+        marginVertical: 10
+    },
+    listTitle:{
         backgroundColor: '#ccc',
         borderColor: 'black',
-        borderWidth: 1
+        borderWidth: 1,
+        flex:4,
+        height:'100%',
+        alignItems: 'center',
+        marginRight:10
+    },
+    delButton:{
+        flex:1,
+        
     }
 });
 export default GoalItem;
