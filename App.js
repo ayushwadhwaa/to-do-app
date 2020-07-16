665c349668c6e39a6aae16010fc21864482644b6
Ayush Wadhwa <ayush_wadhwa@yahoo.com>
2020-07-16 22:17:06 +0530

    Added GoalInput in app.js

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import React, {useState} from 'react';
+import {View, StyleSheet, Button} from 'react-native'
+import GoalInput from './Components/GoalInput'
+export default function App(){
+  const [isAddMode, setIsAddMode] = useState(false);
+  return(
+    <View style={styles.screen}> 
+        <Button title="Add New Goal" onPress={setIsAddMode(true)}/>
+        <GoalInput
+          visible={isAddMode}
+        />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  screen:{
+    padding: 50
+  }
+})
